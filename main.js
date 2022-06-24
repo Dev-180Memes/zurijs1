05890689cc9e179b53a2070ac4fecf017cc5ebc7
Adeoluwa Agbakosi <adeoluwaagbakosi@gmail.com>
2022-06-24 10:15:56 +0000

    Project completed

---
+++ main.js
@@ -0,0 +1,9 @@
+const project = (name, height, country) => {
+    console.log(`Name: ${name}`);
+    console.log(`Height: ${height}`);
+    console.log(`Country: ${country}`);
+};
+
+project("Adeoluwa", 5.10, "Nigeria");
+
+// Using functions make the code reuasble for any number of cases.
